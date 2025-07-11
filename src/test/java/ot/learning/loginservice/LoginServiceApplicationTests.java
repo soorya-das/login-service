2b23a535a1d394de1cfdb59bcbba8aefeaabe1a1
soorya-das <sooryadasks22@gmail.com>
2025-07-11 15:25:07 +0530

    Initial commit

---
+++ src/test/java/ot/learning/loginservice/LoginServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ot.learning.loginservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
